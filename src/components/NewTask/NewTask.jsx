123938670675b40e976cef4caf0e5acdd6edd43e
Stanislav Nemytov <staninspain69@gmail.com>
2021-03-07 20:07:58 +0100

    Added notification aftre creating a new task.

--- src/components/NewTask/NewTask.jsx
+++ src/components/NewTask/NewTask.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-console */
 /* eslint-disable no-shadow */
-import { Button, Form, Input } from "antd";
+import { Button, Col, Form, Input, notification, Row } from "antd";
 import { useForm } from "antd/lib/form/Form";
+import Text from "antd/lib/typography/Text";
+import Title from "antd/lib/typography/Title";
 import { React } from "react";
 import { connect } from "react-redux";
 import { getPage, sendNewTask, startLoading } from "../../store/requests";
@@ -36,10 +38,19 @@ function NewTask({ sendNewTask, startLoading, getPage, reducerAPI }) {
   const { params } = reducerAPI;
   const [form] = useForm();
 
+  const openNotification = (text) => {
+    notification.open({
+      message: "Task was added!",
+      description: <Text>{text}</Text>,
+      placement: "bottomRight",
+    });
+  };
+
   const onFinish = async (values) => {
     startLoading();
-    await sendNewTask(values);
+    sendNewTask(values);
     form.resetFields();
+    openNotification(values.text);
     getPage(params);
   };
 
@@ -48,63 +59,74 @@ function NewTask({ sendNewTask, startLoading, getPage, reducerAPI }) {
   };
 
   return (
-    <Form
-      {...layout}
-      name="task"
-      form={form}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      scrollToFirstError
-    >
-      <Form.Item
-        name="email"
-        label="Email"
-        rules={[
-          {
-            type: "email",
-            message: "The input is not valid E-mail!",
-          },
-          {
-            required: true,
-            message: "Please input your E-mail!",
-          },
-        ]}
+    <>
+      <Row>
+        <Col
+          lg={{
+            offset: 4,
+          }}
+        >
+          <Title level={4}>Create a new task</Title>
+        </Col>
+      </Row>
+      <Form
+        {...layout}
+        name="task"
+        form={form}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        scrollToFirstError
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          name="email"
+          label="Email"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
+            },
+            {
+              required: true,
+              message: "Please input your E-mail!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        name="username"
-        label="Username"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Username!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          name="username"
+          label="Username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        name="text"
-        label="Task"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Task!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          name="text"
+          label="Task"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Task!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 }
 
