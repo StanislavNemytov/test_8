574807e0b605434e10d5cbb422237a40ca378940
Stanislav Nemytov <staninspain69@gmail.com>
2021-03-07 18:28:04 +0100

    Added connection woth two and more tabs.

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,18 @@
 import { applyMiddleware, compose, createStore, combineReducers } from "redux";
 import thunkMiddleware from "redux-thunk";
+import {
+  createStateSyncMiddleware,
+  initStateWithPrevTab,
+  withReduxStateSync,
+} from "redux-state-sync";
 import reducerAPI from "./reducers/reducerAPI";
 import reducerAuthorization from "./reducers/reducerAuthorization";
 import reducerText from "./reducers/reducerText";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const config = {};
+const middlewareArr = [createStateSyncMiddleware(config), thunkMiddleware];
 
 const rootReducer = combineReducers({
   reducerAPI,
@@ -13,11 +21,12 @@ const rootReducer = combineReducers({
 });
 
 const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunkMiddleware))
+  withReduxStateSync(rootReducer),
+  composeEnhancers(applyMiddleware(...middlewareArr))
 );
 
 // eslint-disable-next-line no-underscore-dangle
 window.__store__ = store;
+initStateWithPrevTab(store);
 
 export default store;
